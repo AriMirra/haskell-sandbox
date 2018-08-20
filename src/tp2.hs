7a8be3368689a3cb7ee4d9aa8374af9b137b9a9b
arielmirra <ariel.mirra@ing.austral.edu.ar>
2018-08-20 14:07:30 -0300

    add binary tree data structure and methods

--- src/tp2.hs
+++ src/tp2.hs
@@ -58,3 +58,22 @@ type Binary = [Bit]
 -- bitsum _ _ = (_,_) 
 
 
+-- 2. Definir un tipo arbol (Tree) y definir una función de construcción
+data Tree a = EmptyTree | Node a (Tree a) (Tree a) deriving (Show, Eq, Read)
+
+singleton :: a -> Tree a
+singleton x = Node x EmptyTree EmptyTree
+
+treeInsert :: (Ord a) => a -> Tree a -> Tree a
+treeInsert x EmptyTree = singleton x
+treeInsert x (Node a left right)
+    | x == a = Node x left right
+    | x < a  = Node a (treeInsert x left) right
+    | x > a  = Node a left (treeInsert x right)
+
+treeElem :: (Ord a) => a -> Tree a -> Bool
+treeElem _ EmptyTree = False
+treeElem x (Node a left right)
+    | x == a = True
+    | x < a  = treeElem x left
+    | x > a  = treeElem x right
